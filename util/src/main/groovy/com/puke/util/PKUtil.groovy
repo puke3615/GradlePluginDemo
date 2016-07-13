2f86a7587f1f4a1f255388ba1bbf29d9125477fb
zijiao <zijiao@mogujie.com>
2016-07-14 00:36:24 +0800

    dependOn

--- util/src/main/groovy/com/puke/util/PKUtil.groovy
+++ util/src/main/groovy/com/puke/util/PKUtil.groovy
@@ -7,6 +7,10 @@ class ReplaceName {
     String newName
 }
 
+class Dog {
+    int age
+}
+
 
 class PKUtil implements Plugin<Project> {
 
@@ -18,5 +22,30 @@ class PKUtil implements Plugin<Project> {
         //rename task
         project.extensions.create('rename', ReplaceName)
         project.task('pk_rename', type: RenameTask)
+
+        //test task
+        project.extensions.create('xiaomo', Dog)
+        def test = project.tasks.getByName('pk_test')
+        test.onlyIf {
+            return project.extensions.xiaomo.age > 5
+        }
+        test.doFirst {
+            println 'pre_1'
+        }.doFirst {
+            println 'pre_2'
+        }.doLast {
+            println 'post_1'
+        }.doLast {
+            println 'post_2'
+        }
+
+        //upload
+        def upload = project.tasks.getByName('uploadArchives')
+        upload.doFirst {
+            project.group = 'com.puke'
+            project.artifacts.properties.each {
+                println "${it.key}:${it.value}"
+            }
+        }
     }
 }
